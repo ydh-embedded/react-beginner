9ea37c26631d8a3dc80add7271bf2b30d93dc88a
DHE <ydh.postkasten@yahoo.com>
2024-02-22 10:49:28 +0100

    dev

--- update.bat
+++ update.bat
@@ -1,3 +1,10 @@
-cd .\frontend-developement     REM relativer Pfad Angabe vom . aktuellen Verzeichniss 
-git pull                        REM @echo off kann man den bearbeitunsstand ausblenden
+REM relativer Pfad Angabe vom . aktuellen Verzeichniss  cd .\frontend-developement
+REM @echo off kann man den bearbeitunsstand ausblenden
+REM absoluter Pfad wenn man sich in einem anderen Verzeichniss befindet zum Beipiel im shell:startup Verzeichniss
+REM C:\Users\danny.heinicke\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+
+REM Wir gehen als aller Erstes in den Ordner wo wir git ausführen wollen und führen den Befehl git pull aus
+cd C:\Users\danny.heinicke\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+git pull                        
 pause
