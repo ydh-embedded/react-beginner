86e50a184b3f5ef4fd1adb5b487bedf8c9194a33
DHE <ydh.postkasten@yahoo.com>
2024-02-22 10:36:35 +0100

    dev batch

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 cd .\frontend-developement     REM relativer Pfad Angabe vom . aktuellen Verzeichniss 
-git pull
+git pull                        REM @echo off kann man den bearbeitunsstand ausblenden
 pause
